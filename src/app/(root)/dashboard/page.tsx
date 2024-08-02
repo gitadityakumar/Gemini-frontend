02d475822fe89e4d582b51ce7a9d3dedca889cc7
gitadityakumar <itsadityasingh05@gmail.com>
2024-08-02 08:32:49 +0530

    resolved page issue

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+         <h1 className='text-red-700 text-2xl'> Hi this is dashboard page . </h1>
+        </div> 
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/ui/pageheader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageHeader = () => {
+  return (
+    <main>
+      <div>Hi There </div>
+    </main>
+  )
+}
+
+export default PageHeader
